e709b82650d89c59efe354ac12ec6cebc7f4afbe
sali-k <saki.kimura78@gmail.com>
2020-11-23 09:15:28 +0900

    画像ボタン表示

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import logo from "./assets/egg01.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.comment}>割れると思うなよ</Text>
+      <Pressable
+        onPress={() => alert("Hello, world!")}
+        // style={{ backgroundColor: "blue" }}
+      >
+        <Image source={logo} style={styles.logo}></Image>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +24,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  logo: {
+    width: 300,
+    height: 300,
+    marginBottom: 150,
+    resizeMode: "contain",
+  },
+
+  comment: {
+    width: 400,
+    height: 100,
+    backgroundColor: "#000",
+    marginBottom: 200,
+    fontSize: 50,
+    textAlign: "center",
+    color: "#0f0",
   },
 });
